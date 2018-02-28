4ecd8639c66a5a54b799847666daf1b91188bd57
Guadalupe97 <lumoranmtz@gmail.com>
2018-02-28 14:19:14 -0600

    Initial Commit

--- 2-JS-Basics/1/script.js
+++ 2-JS-Basics/1/script.js
@@ -14,6 +14,7 @@ console.log(fullAge);
 */
 
 // Lecture: Variables 2
+/*
 var name = 'John';
 var age = 26;
 
@@ -42,6 +43,33 @@ console.log(name + ' is a ' + age + ' years old ' + job + ' Is he Married? ' + i
 //console.log(lastName);
 
 alert(name + ' is a ' + age + ' years old ' + job + ' Is he Married? ' + isMarried + '.');
+*/
+
+//Lecture: operators
+var now = 2016;
+var birthYear = now - 26;
+
+birthYear = now - 26 * 2;
+//2016 - 52
+//1964
+
+console.log(birthYear);
+
+
+
+var ageJohn = 30;
+var ageMark = 30;
+
+ageJohn = ageMark = (3 + 5) * 4 - 6;
+//ageJohn = ageMark = 26
+//ageJohn = 26
+
+ageJohn++;
+ageMark *= 2;
+ageMark = ageMark * 2;
+
+console.log(ageJohn);
+console.log(ageMark);
 
 
 
