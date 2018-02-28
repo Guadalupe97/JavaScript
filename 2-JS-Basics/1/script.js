31677e5dc2e513505c67f21d0f55d1c930607276
Guadalupe97 <lumoranmtz@gmail.com>
2018-02-28 13:58:21 -0600

    Initial Commit

--- 2-JS-Basics/1/script.js
+++ 2-JS-Basics/1/script.js
@@ -1,3 +1,5 @@
+//Lecture: Variables
+/*
 var name = 'John';
 console.log(name);
 
@@ -9,3 +11,39 @@ console.log(age);
 
 var fullAge = true;
 console.log(fullAge);
+*/
+
+// Lecture: Variables 2
+var name = 'John';
+var age = 26;
+
+//console.log(name + age);
+//console.log(age + age);
+
+
+var job, isMarried;
+
+//console.log(job);
+
+job = 'teacher';
+isMarried = false;
+
+console.log(name + ' is a ' + age + ' years old ' + job + ' Is he Married? ' + isMarried + '.');
+
+
+
+age = 'thirty six';
+job = 'driver';
+
+console.log(name + ' is a ' + age + ' years old ' + job + ' Is he Married? ' + isMarried + '.');
+
+
+//var lastName = prompt('Whats is the last name?');
+//console.log(lastName);
+
+alert(name + ' is a ' + age + ' years old ' + job + ' Is he Married? ' + isMarried + '.');
+
+
+
+
+
