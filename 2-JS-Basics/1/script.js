3d86cee8a5d13a6b9f78641c87fd057548979787
Guadalupe97 <lumoranmtz@gmail.com>
2018-03-01 10:49:04 -0600

    Initial Commit

--- 2-JS-Basics/1/script.js
+++ 2-JS-Basics/1/script.js
@@ -317,7 +317,7 @@ var john = {
     */
 
     //V 2.0
-
+    /*
     var john = {
     name: 'John',
     lastName: 'Smith',
@@ -346,5 +346,64 @@ var john = {
     console.log(mike);
 
 
+*/
+
+//Lecture: Loops
+
+    for (var i = 0; i < 10; i++){
+        console.log(i);
+    }
+    
+    /*
+    0, true, print 0, update i to 1
+    1, true, print 0, update i to 2
+    .
+    .
+    .
+    9, true, print 0, update i to 10
+    10, False, end loop!
+    */
+    
+    var names = ['John', 'Jane', 'Mary', 'Mark', 'Bob'];
+
+    // for loops
+    /*
+    for (var i = 0; i < names.length; i++){
+        console.log(names[i]);
+    }
+    
+    for (var i = names.length - 1; i >= 0; i--){
+        console.log(names[i]);
+    }
+    */
+
+    //while loops
+    var i = 0;
+    while (i < names.length){
+        console.log(names[i]);
+        i++
+    } 
+    
+    for (var i = 1; i <=5; i++){
+        console.log(i);
+        
+        if (i === 3){
+        break;    
+        }
+    }
+
+    for (var i = 1; i <=5; i++){        
+        if (i === 3){
+            continue;    
+        }
+        console.log(i);
+    }
 
     
+
+
+
+    
+    
+    
+     
