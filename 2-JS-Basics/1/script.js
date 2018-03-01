f128d4f76c30ee61b959c4df27bb6d2b62cae93e
Guadalupe97 <lumoranmtz@gmail.com>
2018-03-01 10:14:36 -0600

    Initial Commit

--- 2-JS-Basics/1/script.js
+++ 2-JS-Basics/1/script.js
@@ -145,7 +145,7 @@ switch(job){
 */
 
 //SOLUTION 
-
+/*
 var heightJohn = 180;
 var heigtMike = 165;
 var ageJohn = 26;
@@ -192,7 +192,7 @@ else {
 */
 
 //Lecture: Funcions
-
+/*
 function calculateAge(yearOfBirth){
  var age = 2016 - yearOfBirth;
     return age;
@@ -218,3 +218,133 @@ function yearsUntilRetirement(name, year){
 yearsUntilRetirement('John', 1990);
 yearsUntilRetirement('Mike', 1969);
 yearsUntilRetirement('Mary', 1948);
+
+*/
+//Lecture: Statements and expressions
+/*
+function someFun(par){
+    //code
+}
+var someFun = function(par){
+    //code
+}
+
+//Expressions 
+3 + 4;
+var x = 3;
+
+//statements
+
+if (x === 5){
+    //do something
+}
+*/
+// Arrays
+/*
+var names = ['John', 'Jane', 'Mark'];
+var years = new Array (1990, 1969, 1948);
+        
+console.log(names[2]);
+names[1] = 'Ben';
+console.log(names);
+
+var john = ['John', 'Smith', 1990, 'designer', false];
+
+john.push('blue');
+john.unshift('Mr.');
+john.pop();
+john.shift();
+console.log(john);
+
+if(john.indexOf('teacher') === -1){
+    console.log('John is NOT a teacher');
+}
+ */
+
+//Lecture: Objects
+/*
+var john = {
+    name: 'John',
+    lastName: 'Smith',
+    yearOfBirth: 1990,
+    job: 'teacher',
+    isMarried: false
+};
+
+console.log(john.lastName);
+console.log(john['lastName']);
+
+var xyz = 'job';
+console.log(john[xyz]);
+
+john.lastName = 'Miller';
+john['job'] = 'programmer';
+
+console.log(john);
+
+var jane = new Object();
+jane.name = 'Jane';
+jane.lastName = 'Smith';
+jane['yearOfBirth'] = 1969;
+jane['job'] = 'retired';
+jane['isMarried'] = true;
+
+console.log(jane);
+*/
+
+//Lecture: Objects and methods
+// v 1.0
+/*
+var john = {
+    name: 'John',
+    lastName: 'Smith',
+    yearOfBirth: 1990,
+    job: 'teacher',
+    isMarried: false,
+    family: ['Jane', 'Mark', 'Bob'],
+    calculateAge: function(){
+        return 2016 - this.yearOfBirth;
+}
+};
+
+    //console.log(john.calculateAge(1970));
+    console.log(john.calculateAge());
+
+    var age = john.calculateAge();
+    john.age = age;
+    
+    console.log(john);
+    */
+
+    //V 2.0
+
+    var john = {
+    name: 'John',
+    lastName: 'Smith',
+    yearOfBirth: 1990,
+    job: 'teacher',
+    isMarried: false,
+    family: ['Jane', 'Mark', 'Bob'],
+    calculateAge: function(){
+        //return 2016 - this.yearOfBirth;
+        this.age = 2016 - this.yearOfBirth;
+}
+}  ;
+    
+    john.calculateAge();
+    console.log(john);
+
+ var mike = {
+    yearOfBirth: 1950,
+    calculateAge: function(){
+        //return 2016 - this.yearOfBirth;
+        this.age = 2016 - this.yearOfBirth;
+}
+}  ;
+    
+    mike.calculateAge();
+    console.log(mike);
+
+
+
+    
