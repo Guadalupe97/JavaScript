f747114ec3652202fcef1bc7541a47b805e229e7
Guadalupe97 <lumoranmtz@gmail.com>
2018-03-02 11:50:36 -0600

    Initial Commit

--- 5-Advanced-JS/script.js
+++ 5-Advanced-JS/script.js
@@ -35,7 +35,7 @@ console.log(mark.lastName);
 */
 
 //Object.create
-
+/*
 var personProto = {
     
     calculateAge: function(){
@@ -49,12 +49,52 @@ john.name = 'John';
 john.yearOfBirth = 1990;
 john.job = 'teacher';
 
-var jane = Object.create(personProto,                      {
+var jane = Object.create(personProto, {
     name: { value: 'Jane'},
     yearOfBirth: { value: 1969},
     job: { value: 'designer'}
 });
 
+*/
+
+//Primitives vs objects
+
+//Primitivos
+var a = 23;
+var b = a;
+a = 46;
+
+console.log(a);
+console.log(b);
 
 
+//Objetos
 
+var obj1 = {
+    name: 'john',
+    age: 26
+};
+
+var obj2 = obj1;
+obj1.age = 30;
+
+console.log(obj1.age);
+console.log(obj2.age);
+
+
+//Functions
+
+var age = 27;
+var obj = {
+    name: 'Jonas',
+    city: 'Lisbon'
+};
+
+function change(a, b){
+    a = 30;
+    b.city = 'San Francisco';
+}
+
+change(age, obj);
+console.log(age);
+console.log(obj.city);
